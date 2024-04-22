1799c58ac40ec18df908a305c39ce3a9717448b2
dhzdhd <ragas5432100@gmail.com>
2024-04-22 10:19:23 +0530

    Add volume

--- scripts/run-server.ps1
+++ scripts/run-server.ps1
@@ -3,11 +3,14 @@ docker compose -f .\docker\docker-compose-server.yml up -d
 
 Write-Output "Execute example"
 docker exec -it spark /opt/spark/bin/spark-submit `
-    --master k8s://http://<ip>:6443 `
+    --master k8s://https://<ip>:6443 `
     --deploy-mode cluster `
     --name spark-pi `
     --class org.apache.spark.examples.SparkPi `
     --conf spark.executor.instances=2 `
     --conf spark.kubernetes.container.image=spark `
     --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark `
+    --conf spark.kubernetes.authenticate.submission.oauthToken= `
+    --conf spark.kubernetes.authenticate.submission.caCertFile=/workdir/client-ca.crt `
+    --conf spark.kubernetes.authenticate.submission.clientCertFile=/workdir/client-ca.crt `
     local:///opt/spark/examples/jars/spark-examples_2.12-3.5.1.jar
